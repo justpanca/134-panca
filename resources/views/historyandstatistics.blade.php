e451267f6f15b465be1fc92dd8d5519d240fe323
justpanca <pancafantastic6@gmail.com>
2024-05-13 21:58:33 +0700

    personal project layouting

---
+++ resources/views/historyandstatistics.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+<x-slot:title>{{ $title  }}</x-slot:title>
+<h3 class="text-xl">welcome to my history</h3>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+<x-slot:title>{{ $title  }}</x-slot:title>
+<h3 class="text-xl">welcome to my homepage</h3>
+</x-layout>
